0a91583065bc6f5becba54ec94bd1bd632fbb2bd
sohilpandya <sohilpandya@me.com>
2015-09-25 11:25:20 +0100

    addressed testing issues and searching issues

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-/*var sckey = 38fed2acd8bf8e0001c5a3dddad41c76;*/
-=======
-var sckey = '38fed2acd8bf8e0001c5a3dddad41c76'
->>>>>>> gh-pages
--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 
 
-  test('Has the button been clicked', function(assert) {
+  test('Has the Search button been clicked. You have to physically click the search button within the first 2 seconds to make this test pass', function(assert) {
     var done = assert.async();
     setTimeout (function(){
       var submitButton = document.getElementById('search');
       submitButton.addEventListener("click", function(){
         clicked = true;
       });
-      assert.equal( clicked, true, 'Button has been clicked' );
+      assert.equal( clicked, true, 'Search Button has been clicked' );
       done();
     },2000);
   });
 
-  test('Have result been returned when search is clicked', function(assert) {
+  test('Have Instagram results been returned when search is clicked', function(assert) {
     var done = assert.async();
     var check = false;
     setTimeout (function(){
@@ -20,22 +20,22 @@
     if(document.getElementsByTagName('img')){
         check = true;
       }
-      assert.equal( check, true, 'Images are showing' );
+      assert.equal( check, true, 'Instagram Images are showing' );
       done();
     },1000);
   });
 
-  test("when search is clicked return response", function(assert) {
+  test("This is an auto search test, user will not need to click search button for this test to pass. It will automatically return Instagram results with foifighters.", function(assert) {
     var done = assert.async();
     setTimeout(function(){
       // clickfunc();
       // console.log(clickfunc());
-      assert.equal(clickfunc(),'foifighters');
+      assert.equal(clickfunc(),'foifighters','Instagram search returns six photos with the entered search result');
       done();
     },1000);
   });
 
-  test('Have result been returned when search is clicked', function(assert) {
+  test('Have Soundcloud results been returned when search is clicked', function(assert) {
   	var done = assert.async();
   	var check = false;
   	setTimeout (function(){
@@ -43,12 +43,12 @@
   	if(document.getElementById("track").innerHTML){
   			check = true;
   		}
-  			assert.equal( check, true, 'Tracks are available' );
+  			assert.equal( check, true, 'Soundcloud Track is available for play' );
   			done();
   		},1000);
   		});
 
-  	test('Has the button been clicked?', function(assert) {
+  	test('Has the Soundcloud Play button been clicked?, User will need to manually click the play button for this test to pass. ', function(assert) {
   			var clicked = false;
   			var done = assert.async();
 
@@ -57,7 +57,7 @@
           clicked = true;
        });
   	    setTimeout (function(){
-  	      assert.equal( clicked, true, 'Button has been clicked' );
+  	      assert.equal( clicked, true, 'Play Button has been clicked' );
   	      done();
   	    },5000);
   	});
