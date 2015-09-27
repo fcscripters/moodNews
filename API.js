3b00ada4f11c4a5f2272cf877194cf49be6a0071
tormod17 <tormodsmith@gmail.com>
2015-09-27 02:23:23 +0100

    changes to SC selector

--- API.js
+++ API.js
@@ -1,1 +1,54 @@
-function displayData(e){for(var t=[],n=[],c=0;20>c;c++)n.push(e.data[c]);n.sort(function(e,t){return t.likes.count-e.likes.count});for(var a=0;6>a;a++)t.push(n[a].images.standard_resolution.url);for(var o=0;6>o;o++){var d=document.createElement("img");d.setAttribute("src",t[o]),d.setAttribute("class","instaphotos"),d.setAttribute("configurable",!0),d.setAttribute("alt","na"),document.getElementById("insta").appendChild(d)}}function clickfunc(){var e=document.getElementById("result").value,t=e.replace(/\s+/g,"");clicked=!0,""===t&&(t="foifighters");var n=document.createElement("script");n.src="https://api.instagram.com/v1/tags/"+t+"/media/recent?access_token=2208596365.1fb234f.2d3cc38a6e354a958800809ced644f50&count=33&callback=displayData",document.body.appendChild(n);var c=document.getElementsByClassName("instaphotos");for(index=c.length-1;index>=0;index--)c[index].parentNode.removeChild(c[index]);return console.log(t),t}function soundfunc(){SC.initialize({client_id:"5c5a64cfdf37c482bb757df270d33c53",redirect_uri:"http://fcscripters.github.io/moodNews/callback.html"});var e,t=document.getElementById("result").value;SC.get("/tracks",{q:t},function(t){t.sort(function(e,t){return e.created_at<t.created_at});for(var n=0;n<t.length;n++)console.log(t[n].created_at);console.log(t),e=t[0].id,document.getElementById("track").innerHTML="<a href="+t[0].permalink_url+">"+t[0].title+"</a>",document.getElementById("trackUser").innerHTML=t[0].user.username,SC.stream("/tracks/"+e,function(t){document.getElementById("play").addEventListener("click",function(){console.log(e),t.play()}),document.getElementById("stop").addEventListener("click",function(){console.log(e),t.stop()}),document.getElementById("pause").addEventListener("click",function(){console.log(e),t.pause()})})})}function hide(){var e=document.getElementById("hidetest");"none"!=e.style.display?e.style.display="none":e.style.display="block"}var clicked=!1;document.getElementById("search").addEventListener("click",clickfunc),document.getElementById("search").addEventListener("click",soundfunc);
+function displayData(e) {
+    for (var t = [], n = [], c = 0; 20 > c; c++) n.push(e.data[c]);
+    n.sort(function(e, t) {
+        return t.likes.count - e.likes.count
+    });
+    for (var a = 0; 6 > a; a++) t.push(n[a].images.standard_resolution.url);
+    for (var o = 0; 6 > o; o++) {
+        var d = document.createElement("img");
+        d.setAttribute("src", t[o]), d.setAttribute("class", "instaphotos"), d.setAttribute("configurable", !0), d.setAttribute("alt", "na"), document.getElementById("insta").appendChild(d)
+    }
+}
+
+function clickfunc() {
+    var e = document.getElementById("result").value,
+        t = e.replace(/\s+/g, "");
+    clicked = !0, "" === t && (t = "foifighters");
+    var n = document.createElement("script");
+    n.src = "https://api.instagram.com/v1/tags/" + t + "/media/recent?access_token=2208596365.1fb234f.2d3cc38a6e354a958800809ced644f50&count=33&callback=displayData", document.body.appendChild(n);
+    var c = document.getElementsByClassName("instaphotos");
+    for (index = c.length - 1; index >= 0; index--) c[index].parentNode.removeChild(c[index]);
+    return console.log(t), t
+}
+
+function soundfunc() {
+    SC.initialize({
+        client_id: "5c5a64cfdf37c482bb757df270d33c53",
+        redirect_uri: "http://fcscripters.github.io/moodNews/callback.html"
+    });
+    var e, t = document.getElementById("result").value;
+    SC.get("/tracks", {
+        q: t
+    }, function(t) {
+        t.sort(function(e, t) {
+            return e.created_at < t.created_at
+        });
+        for (var n = 0; n < t.length; n++) console.log(t[n].created_at);
+        console.log(t), e = t[0].id, document.getElementById("track").innerHTML = "<a href=" + t[0].permalink_url + ">" + t[0].title + "</a>", document.getElementById("trackUser").innerHTML = t[0].user.username, SC.stream("/tracks/" + e, function(t) {
+            document.getElementById("play").addEventListener("click", function() {
+                console.log(e), t.play()
+            }), document.getElementById("stop").addEventListener("click", function() {
+                console.log(e), t.stop()
+            }), document.getElementById("pause").addEventListener("click", function() {
+                console.log(e), t.pause()
+            })
+        })
+    })
+}
+
+function hide() {
+    var e = document.getElementById("hidetest");
+    "none" != e.style.display ? e.style.display = "none" : e.style.display = "block"
+}
+var clicked = !1;
+document.getElementById("search").addEventListener("click", clickfunc), document.getElementById("search").addEventListener("click", soundfunc);
--- APIfull.js
+++ APIfull.js
@@ -3,65 +3,65 @@
 //
 
 function displayData(response) {
-  var arrphotos = [];
-  var arrphotoswhole = [];
+    var arrphotos = [];
+    var arrphotoswhole = [];
 
-  for (var s = 0; s < 20; s++) {
-    arrphotoswhole.push(response['data'][s]);
-  }
+    for (var s = 0; s < 20; s++) {
+        arrphotoswhole.push(response['data'][s]);
+    }
 
-  arrphotoswhole.sort(function(a,b){
-    return b['likes']['count'] - a['likes']['count'];
-  });
+    arrphotoswhole.sort(function(a, b) {
+        return b['likes']['count'] - a['likes']['count'];
+    });
 
-  for (var i = 0; i < 6; i++) {
-    arrphotos.push(arrphotoswhole[i]['images']['standard_resolution']['url']);
+    for (var i = 0; i < 6; i++) {
+        arrphotos.push(arrphotoswhole[i]['images']['standard_resolution']['url']);
 
-  }
+    }
 
-  // document.getElementById("pic").setAttribute("src", imgArray[0]);
-  for (var j = 0; j < 6; j++) {
-    var oImg = document.createElement("img");
-    oImg.setAttribute('src', arrphotos[j]);
-    oImg.setAttribute('class', 'instaphotos');
-    oImg.setAttribute('configurable', true);
+    // document.getElementById("pic").setAttribute("src", imgArray[0]);
+    for (var j = 0; j < 6; j++) {
+        var oImg = document.createElement("img");
+        oImg.setAttribute('src', arrphotos[j]);
+        oImg.setAttribute('class', 'instaphotos');
+        oImg.setAttribute('configurable', true);
 
-    oImg.setAttribute('alt', 'na');
-    // document.body.appendChild(oImg);
-    document.getElementById('insta').appendChild(oImg);
-    // document.getElementById('instagramphotos').innerHTML = '<ol><li>'oImg'</li></ol>';
+        oImg.setAttribute('alt', 'na');
+        // document.body.appendChild(oImg);
+        document.getElementById('insta').appendChild(oImg);
+        // document.getElementById('instagramphotos').innerHTML = '<ol><li>'oImg'</li></ol>';
 
-    // document.getElementById('instagramphotos').appendChild(oImg);
+        // document.getElementById('instagramphotos').appendChild(oImg);
 
-  }
+    }
 }
 var clicked = false;
 
 
 
-function clickfunc(){
-  var str = document.getElementById('result').value;
-  var clientS = str.replace(/\s+/g, '');
+function clickfunc() {
+    var str = document.getElementById('result').value;
+    var clientS = str.replace(/\s+/g, '');
 
-  clicked = true;
+    clicked = true;
 
-  // create script element
-  if (clientS === ''){
-    clientS = 'foifighters';
-  }
-  var script = document.createElement('script');
-  // assing src with callback name
-  script.src = "https://api.instagram.com/v1/tags/" + clientS + "/media/recent?access_token=2208596365.1fb234f.2d3cc38a6e354a958800809ced644f50&count=33&callback=displayData";
-  // insert script to document and load content
-  document.body.appendChild(script);
+    // create script element
+    if (clientS === '') {
+        clientS = 'foifighters';
+    }
+    var script = document.createElement('script');
+    // assing src with callback name
+    script.src = "https://api.instagram.com/v1/tags/" + clientS + "/media/recent?access_token=2208596365.1fb234f.2d3cc38a6e354a958800809ced644f50&count=33&callback=displayData";
+    // insert script to document and load content
+    document.body.appendChild(script);
 
-  //this removes img elements
-  var element = document.getElementsByClassName("instaphotos");
-  for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-  }
-  console.log(clientS);
-  return clientS;
+    //this removes img elements
+    var element = document.getElementsByClassName("instaphotos");
+    for (index = element.length - 1; index >= 0; index--) {
+        element[index].parentNode.removeChild(element[index]);
+    }
+    console.log(clientS);
+    return clientS;
 }
 //console.log(clickfunc());
 document.getElementById("search").addEventListener("click", clickfunc);
@@ -72,39 +72,98 @@ document.getElementById("search").addEventListener("click", clickfunc);
 //
 // Soundcloud API
 //
+SC.initialize({
 
+    client_id: '5c5a64cfdf37c482bb757df270d33c53',
+    redirect_uri: 'http://fcscripters.github.io/moodNews/callback.html'
+
+});
 
 
 
 document.getElementById("search").addEventListener("click", soundfunc);
 
-function soundfunc(){
-  // initialize client with app credentials */
-  SC.initialize({
+function soundfunc() {
+    var query = document.getElementById("result").value;
+    var tracks = 0;
 
-    /* client_id: '38fed2acd8bf8e0001c5a3dddad41c76',*/
-    client_id: '5c5a64cfdf37c482bb757df270d33c53',
-    redirect_uri: 'http://fcscripters.github.io/moodNews/callback.html'
+    if (query == null) {
+        query = 'beatles'
+    }
 
-  });
+    SC.get("/tracks", {
+            q: query,
+            limit: 3
+        },
+        function getTracksObjfromSC(tracks) {
+            console.log(tracks);
 
-  var query = document.getElementById("result").value;
-  // var query = "oasis";
-  var trackID;
 
 
-  SC.get("/tracks", {
+            function addTitleUsername() {
+                for (i = 0; i < tracks.length; i++) {
+                    var titleAndUsername = document.getElementById('title')
+                    title.innerHTML += '<li id="' + tracks[i].id + '" class="tracks">' + tracks[i].title + '</li><li>' + tracks[i].user.username + '</li>';
+                }
 
-    q: query,
-    // created_at: {'from': todayDate},
-    //limit: 7
-  }, function(tracks) {
+            }
 
-tracks.sort(function(a,b){
-  return a.created_at < b.created_at;
-});
 
-//Adding track name and user URL to soundcloud div
+            function playPauseStop(trackID) {
+                SC.stream('/tracks/' + trackID, function(sound) {
+
+                    document.getElementById('play').addEventListener("click", function() {
+                        sound.play();
+                    });
+                    document.getElementById('stop').addEventListener("click", function() {
+                        sound.stop();
+                    })
+                    document.getElementById('pause').addEventListener("click", function() {
+                        sound.pause();
+                    });
+                });
+            }
+           
+           function onlySelectOne(elements){
+                 for(i=0; i<elements.length; i++){
+                  console.log(elements[i])
+                    if ( elements[i].className =="selected"){
+                         elements[i].className ="tracks";
+                    }
+                 }
+            }
+
+
+           function chooseSongbyClicking() {
+                var songs = document.getElementsByClassName("tracks");
+                for (var i = 0; i < songs.length; i++) {
+                    songs[i].onclick = function(e) {
+                         document.getElementById(this.id).className="selected";
+                         playPauseStop(this.id)
+                    }
+                };
+            }
+
+
+            addTitleUsername();
+            chooseSongbyClicking();
+           
+
+
+
+
+
+        });
+
+
+
+
+}
+
+
+
+
+/*//Adding track name and user URL to soundcloud div
     trackID = tracks[0].id;
     //console.log (trackID);
 //these have been created using soundclouds API user guide lines
@@ -113,31 +172,23 @@ tracks.sort(function(a,b){
     document.getElementById("trackUser").innerHTML = tracks[0].user.username;
 
   SC.stream("/tracks/"+ trackID, function(sound) {
-      document.getElementById('play').addEventListener("click", function(){
-          sound.play();
-      });
-      document.getElementById('stop').addEventListener("click", function(){
-          sound.stop();
-      });
-      document.getElementById('pause').addEventListener("click", function(){
-          sound.pause();
-      });
+    
+    
     });
 
 });
 
 
 }
-//this function hides/ shows the avaliable tests
-function hide(){
+*/ //this function hides/ shows the avaliable tests
+function hide() {
 
-	var el = document.getElementById('hidetest');
-	if ( el.style.display != 'none' ) {
-		el.style.display = 'none';
-	}
-	else {
-		el.style.display = 'block';
-	}
+    var el = document.getElementById('hidetest');
+    if (el.style.display != 'none') {
+        el.style.display = 'none';
+    } else {
+        el.style.display = 'block';
+    }
 
 }
 
